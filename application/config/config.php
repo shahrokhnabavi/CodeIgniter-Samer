8401a403f8c78b3327fb21dad4165c0fd4b70916
Sjahriyar <shahriyar@users.noreply.github.com>
2017-08-30 12:35:57 +0200

    Front End 90% finished
    added video header

--- application/config/config.php
+++ application/config/config.php
@@ -25,7 +25,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 
 
-$config['base_url'] = 'http://localhost/samer/public/';
+$config['base_url'] = 'http://localhost/Samer/public/';
 
 
 /*
--- application/config/routes.php
+++ application/config/routes.php
@@ -11,7 +11,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | and its corresponding controller class/method. The segments in a
 | URL normally follow this pattern:
 |
-|	example.com/class/method/id/
+|	example.com/class/method/id/3
 |
 | In some instances, however, you may want to remap this relationship
 | so that a different class/function is called than the one
@@ -57,6 +57,7 @@ $route['admin']           = 'admins/login';
 
 
 $route['admin/content']    = 'post';
+$route['admin/emails']    = 'subscription';
 
 
 
