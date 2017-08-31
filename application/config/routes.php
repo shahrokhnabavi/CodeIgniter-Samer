3f6a4189e71a2ab29b0587bde0562b743ed6bad7
Shahrokh Nabavi <shahrokhnabavi@yahoo.com>
2017-08-31 13:54:13 +0200

    Delete gallery

--- application/config/routes.php
+++ application/config/routes.php
@@ -12,17 +12,12 @@ $route['admin/dashboard'] = 'site/dashboard';
 $route['admin/setting']   = 'site/setting';
 $route['admin/logout']    = 'admins/logout';
 
-
-<<<<<<< .merge_file_a07672
-$route['admin/user([0-9]*|[0-9]*/[0-9]*)'] = 'admins/form/$1/$2';
-$route['admin/delete-user/([0-9]*)']       = 'admins/delete/$1';
-=======
 $route['admin/user([0-9]*|[0-9]*/[0-9]*)']  = 'admins/form/$1/$2';
 $route['admin/delete-user/([0-9]*)']  = 'admins/delete/$1';
 
 $route['admin/gallery([0-9]*|[0-9]*/[0-9]*)']  = 'galleries/form/$1/$2';
 $route['admin/delete-gallery/([0-9]*)']  = 'galleries/delete/$1';
->>>>>>> .merge_file_a03980
+
 
 $route['admin/gallery([0-9]*|[0-9]*/[0-9]*)'] = 'galleries/form/$1/$2';
 $route['admin/delete-gallery/([0-9]*)']       = 'galleries/delete/$1';
@@ -32,15 +27,6 @@ $route['thumb-gallery/([0-9]*)']  = 'galleries/thumbImage/$1';
 // Admin Panel by Sheril
 $route['admin/content']    = 'post';
 $route['admin/delete-content/([0-9]*)']  = 'post/delete/$1';
-<<<<<<< .merge_file_a07672
-
-
-// Admin Panel by Shahriyar
-$route['contact']    = 'contactform';
-$route['admin/emails']    = 'Subscribe/listOfAll/0';
-$route['admin/emails(:num)']    = 'Subscribe/listOfAll/$1';
-$route['admin/delete-emails/(:num)']    = 'Subscribe/delete_member/$1';
-=======
 $route['admin/edit-content/(:num)']  = 'post/edit/$1';
 $route['admin/content_update']    = 'post/update';
 
@@ -50,9 +36,3 @@ $route['home'] = 'front_end';
 $route['gallery'] = 'front_end/gallery';
 $route['about'] = 'front_end/about';
 $route['subscribe'] = 'subscribe/add_member';
-
-
-
-
-
->>>>>>> .merge_file_a03980
