48f208dfaecda0b927c46f8158e9daf0d60bfdb2
Sjahriyar <shahriyar@users.noreply.github.com>
2017-08-31 13:01:53 +0200

    delete all

--- public/assets/js/scripts.js
+++
@@ -1,6 +0,0 @@
-/* swap open/close side menu icons */
-$('[data-toggle=collapse]').click(function(){
-  	// toggle icon
-  	$(this).find("i").toggleClass("glyphicon-chevron-right glyphicon-chevron-down");
-});
-});
