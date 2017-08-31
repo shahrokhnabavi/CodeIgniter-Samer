19865ecc6c1846eab791b2b39df54c7b2afc725a
Shahrokh Nabavi <shahrokhnabavi@yahoo.com>
2017-08-31 15:26:26 +0200

    Add default route

--- application/config/routes.php
+++ application/config/routes.php
@@ -27,6 +27,7 @@ $route['admin/content_update']    = 'post/update';
 
 
 //front end routes
+$route['default_controller'] = 'front_end';
 $route['home'] = 'front_end';
 $route['gallery'] = 'front_end/gallery';
 $route['about'] = 'front_end/about';
