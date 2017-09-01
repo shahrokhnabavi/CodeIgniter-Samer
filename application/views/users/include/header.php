047f258b59bacad9537632536d4372ffa2e24438
Shahrokh Nabavi <shahrokhnabavi@yahoo.com>
2017-09-01 09:15:49 +0200

    updated: home view index

--- application/views/users/include/header.php
+++ application/views/users/include/header.php
@@ -19,6 +19,7 @@
 
 
     <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/style.css');?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/main.css');?>">
   </head>
 
   <body>
