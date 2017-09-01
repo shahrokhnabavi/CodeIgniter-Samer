4ee312abf7669641fabe4d5a4dda0da9cf8673f9
Shahrokh Nabavi <shahrokhnabavi@yahoo.com>
2017-09-01 03:43:55 +0200

    fixed: the bug of user editing

--- application/models/User.php
+++ application/models/User.php
@@ -67,10 +67,14 @@ class User extends CI_Model
      * @param $value
      * @return mixed
      */
-    public function getByField( $field, $value ){
-        $data = array(
-            $field => $value
-        );
+    public function getByField( $field, $value = '' ){
+        if ( is_array($field) )
+            $data = $field;
+        else
+            $data = array(
+                $field => $value
+            );
+
         return $this->db->get_where($this->tbl, $data)->row_array();
     }
 
