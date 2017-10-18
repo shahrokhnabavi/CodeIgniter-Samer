f4c69bf00f6c4a2308a0f36213322773e6d0514f
Shahrokh Nabavi <info@nabavi.nl>
2017-10-18 16:38:23 +0200

    google api key

--- public_html/assets/js/scripts.js
+++ public_html/assets/js/scripts.js
@@ -1,6 +1,6 @@
 /* swap open/close side menu icons */
 $('[data-toggle=collapse]').click(function(){
-  	// toggle icon
-  	$(this).find("i").toggleClass("glyphicon-chevron-right glyphicon-chevron-down");
-});
+      	// toggle icon
+      	$(this).find("i").toggleClass("glyphicon-chevron-right glyphicon-chevron-down");
+    });
 });
